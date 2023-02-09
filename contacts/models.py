957b67734b5e70ce527dced295c43515c008992f
DeZin7 <marcusandre77@icloud.com>
2023-02-08 18:02:25 -0800

    "Creating the models"

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+"""
+MODEL THAT WE ARE GOING TO IMPLEMENT HERE: 
+
+CONTACTS
+id: INT (automatic)
+name: STR * (mandatory)
+surname: STR (optional)
+phone_number: STR * (mandatory)
+email: STR (optional)
+creation_date: DATETIME (automatic)
+description: texto
+category: CATEGORIA (outro model)
+
+ CATEGORY
+ id: INT
+ name: STR * (mandatory)
+
+"""
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255, blank=True) #blank = True, because its optional 
+    phone_number = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    creation_date = models.DateField(default=timezone.now)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING) 
+
+
