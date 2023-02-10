1240987a538725592cf383c1e5328b1a71c2c10f
DeZin7 <marcusandre77@icloud.com>
2023-02-09 22:44:16 -0800

    "adding search field'

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('search', views.search, name = 'search'),
     path('<int:contact_id>', views.see_contact, name = 'see_contact'),
 ]
 
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,6 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
@@ -28,4 +30,34 @@ def see_contact(request, contact_id):
     return render(request, 'contacts/see_contact.html', {
         'contact': contact
     })
+
+
+def search(request):
+        term = request.GET.get('term')
+
+        if term is None or not term:
+             raise Http404
+
+        fields = Concat('name',Value(' '), 'surname')
+
+        contacts = Contact.objects.annotate(
+             full_name=fields
+        ).filter(
+             Q(full_name__icontains=term) | Q(phone_number__icontains=term)
+        )
+
+        print(term)
+
+        # contacts = Contact.objects.order_by('-id').filter(
+        #     Q(name__icontains = term) | Q(surname__icontains=term),
+        #     show=True
+        # )
+        paginator = Paginator(contacts, 2)
+
+        page_number = request.GET.get('p')
+        contacts = paginator.get_page(page_number)
+
+        return render(request, 'contacts/search.html', {
+            'contacts': contacts
+        })
 
